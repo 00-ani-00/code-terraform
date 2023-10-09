0593345b9ce4f70e7a3ca94a59b59174641e617a
aniket <aniketlagad6@gmail.com>
2023-10-09 10:17:19 +0530

    new  file are added

---
+++ enviroment/dev/dev.auto.tf
@@ -0,0 +1,6 @@
+module "new1" {
+  source = "../../resources/vpc"
+
+cidr_vpc="10.10.1.2/16"
+cidr_public = 
+}
---
+++ resources/vpc/var.tf
@@ -0,0 +1,19 @@
+variable "cidr_vpc" {
+  type = string
+}
+variable "cidr_public" {
+  type = string
+}
+
+variable "cidr_private" {
+  type = string
+}
+variable "vpc_tags" {
+  type = string
+}
+variable "public_subnet_tags" {
+  type = string
+}
+variable "private_subnet_tags" {
+  type = string
+}
