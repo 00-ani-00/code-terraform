a461410e81efaf22ad6350cbeb8da11a4f31ea66
aniket <aniketlagad6@gmail.com>
2024-03-06 11:46:09 +0530

    new file is added

--- autoscalling-alb-code/main.tf
+++ autoscalling-alb-code/main.tf
@@ -1,89 +1,13 @@
-resource "aws_vpc" "vpc" {
-  cidr_block = var.cidr
-  tags = {
-    Name = "my_vpc"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.31.0"
+    }
   }
 }
 
-resource "aws_subnet" "sub1" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "172.16.0.0/24"
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "my-sub1"
-  }
-
+provider "aws" {
+  # Configuration options
+  region = "eu-west-1"
 }
-
-resource "aws_subnet" "sub2" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "172.16.1.0/24"
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "my-sub2"
-  }
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name = "my-igw"
-  }
-
-}
-resource "aws_route_table" "rt" {
-  vpc_id = aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-}
-
-resource "aws_route_table_association" "RTa" {
-  subnet_id      = aws_subnet.sub1.id
-  route_table_id = aws_route_table.rt.id
-}
-
-resource "aws_route_table_association" "RTb" {
-  subnet_id      = aws_subnet.sub2.id
-  route_table_id = aws_route_table.rt.id
-}
-
-resource "aws_security_group" "sg" {
-  name        = "my_sg"
-  description = "Allow SSH and HTTP inbound traffic"
-  vpc_id      = aws_vpc.vpc.id
-
-  ingress {
-    description = "SSH from VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
-
-  ingress {
-    description = "HTTP from VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
-
-  tags = {
-    Name = "my-sg"
-  }
-}
-
